2ab2efb35b4db440cd0018a847bb36edd3953ec9
DaividContato <daividcontato3@hotmail.com>
2023-02-18 17:23:09 -0300

    deletando arquivos estudos

--- string1.js
+++
@@ -1,9 +0,0 @@
-// Faça um programa que inicie com dois nomes, e o programa imprima na tela o seguinte dado: Olá, meu nome é (nome 1) e meu partner do CodeClub é (nome2)
-
-
-const nome1 = 'daivid'
-const nome2 = 'cintia'
-
-const names = ` Olá, meu nome é ${nome1} e meu partner do CodeClub é a  ${nome2}`
-
-console.log(names)
